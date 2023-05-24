476ab5aa5a1504e6832e28bf9a82a627f7618654
Владислав Деменев <v.demenev1905@mail.ru>
2023-05-24 12:10:50 +0700

    Вывод даты

---
+++ lab8.js
@@ -0,0 +1,26 @@
+function showDate() {
+    let out = document.getElementById('current-date');
+    let today = new Date();
+    out.innerHTML = 'Дата и время для Русской локали:' + today.toLocaleString('ru-RU');
+
+    let out1 = document.getElementById('current-date1');
+    let today1 = new Date();
+    out1.innerHTML = 'Дата и время для локали ЮАР:' + today1.toLocaleString('af-ZA');
+
+    let out2 = document.getElementById('current-date2');
+    let today2 = new Date();
+    out2.innerHTML = 'Дата и время для локали Египта:' + today2.toLocaleString('ar-EG');
+
+    let out3 = document.getElementById('current-date3');
+    let today3 = new Date();
+    out3.innerHTML = 'Дата и время для греческой локали:' + today3.toLocaleString('el-GR');
+
+    let out4 = document.getElementById('current-date4');
+    let today4 = new Date();
+    out4.innerHTML = 'Дата и время для пакистанской локали:' + today4.toLocaleString('ur-PK');
+
+    let out5 = document.getElementById('current-date5');
+    let today5 = new Date();
+    out5.innerHTML = 'Дата и время для китайской локали:' + today5.toLocaleString('zh');
+
+}
