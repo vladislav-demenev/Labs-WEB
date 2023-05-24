4ef789a4b0317a0dd833c68ee0aabbe337487504
Владислав Деменев <v.demenev1905@mail.ru>
2023-05-24 13:15:36 +0700

    Время

--- lab8.js
+++ lab8.js
@@ -42,3 +42,11 @@ function chistka() {
     result.innerHTML="";
 
 }
+
+function showTime() {
+    let today = new Date();
+    let currentTime = today.toLocaleTimeString('ru-RU');
+    document.getElementById('time').innerHTML = currentTime;
+} 
+
+setInterval(showTime,1000);
